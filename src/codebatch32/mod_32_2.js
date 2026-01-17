3d8be3322b044328d67803bc27609df7c5bd6e23
gitty-killer <gittykiller@outlook.com>
2026-01-18 04:00:27 +0900

    Add code batch 32

---
+++ src/codebatch32/mod_32_2.js
@@ -0,0 +1,13 @@
+function func322(x) {
+  let total = 0;
+  for (let i = 0; i < x; i++) {
+    total += (i * 322) % 97;
+  }
+  return total;
+}
+function main() {
+  const data = [];
+  for (let i = 1; i < 200; i++) data.push(func322(i));
+  console.log("rand-20260118-99247", data.reduce((a,b)=>a+b,0));
+}
+main();
