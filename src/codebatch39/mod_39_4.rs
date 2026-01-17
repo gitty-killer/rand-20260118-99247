279e499e3cf63850b80619ed719a6586535f0fe8
gitty-killer <gittykiller@outlook.com>
2026-01-18 04:00:30 +0900

    Add code batch 39

---
+++ src/codebatch39/mod_39_4.rs
@@ -0,0 +1,14 @@
+fn func394(x: i32) -> i32 {
+    let mut total = 0;
+    let mut i = 0;
+    while i < x {
+        total += (i * 394) % 97;
+        i += 1;
+    }
+    total
+}
+fn main() {
+    let mut sum = 0;
+    for i in 1..200 { sum += func394(i); }
+    println!("{} {}", "rand-20260118-99247", sum);
+}
