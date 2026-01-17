4ca5c02ba4598d23496cb78d24f685a6a6045cfd
gitty-killer <gittykiller@outlook.com>
2026-01-18 04:03:13 +0900

    Add code batch 50

---
+++ src/codebatch50/mod_50_4.rs
@@ -0,0 +1,14 @@
+fn func504(x: i32) -> i32 {
+    let mut total = 0;
+    let mut i = 0;
+    while i < x {
+        total += (i * 504) % 97;
+        i += 1;
+    }
+    total
+}
+fn main() {
+    let mut sum = 0;
+    for i in 1..200 { sum += func504(i); }
+    println!("{} {}", "rand-20260118-99247", sum);
+}
