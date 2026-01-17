216c9b78d31644bd247c23977a99d5dae49d088d
gitty-killer <gittykiller@outlook.com>
2026-01-18 04:00:28 +0900

    Add code batch 35

---
+++ src/codebatch35/mod_35_1.py
@@ -0,0 +1,12 @@
+def func351(x):
+    total = 0
+    for i in range(x):
+        total += (i * 351) % 97
+    return total
+
+def main():
+    data = [func351(i) for i in range(1, 200)]
+    print("rand-20260118-99247", sum(data))
+
+if rand-20260118-99247 == "__main__":
+    main()
