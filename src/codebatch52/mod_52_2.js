0d6565ba9ab07afaa689fa53d8b010447f114ca9
gitty-killer <gittykiller@outlook.com>
2026-01-18 04:03:14 +0900

    Add code batch 52

---
+++ src/codebatch52/mod_52_2.js
@@ -0,0 +1,13 @@
+function func522(x) {
+  let total = 0;
+  for (let i = 0; i < x; i++) {
+    total += (i * 522) % 97;
+  }
+  return total;
+}
+function main() {
+  const data = [];
+  for (let i = 1; i < 200; i++) data.push(func522(i));
+  console.log("rand-20260118-99247", data.reduce((a,b)=>a+b,0));
+}
+main();
